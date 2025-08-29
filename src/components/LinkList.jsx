ae988f96be6cbe11752ede7ae73a17c357a8fe9a
Lucky Mishra <86152604+Luckymishraa@users.noreply.github.com>
2025-08-29 13:21:39 +0530

    feat: Create LinkList component to render items

--- src/components/LinkList.jsx
+++ src/components/LinkList.jsx
@@ -2,10 +2,22 @@ import React from 'react'
 import LinkItem from './LinkItem' //it uses the link item component!
 
 
-// receives the array of links
-const LinkList = () => {
+// receives the array of links as a prop
+const LinkList = ({ links }) => {
   return (
-    <div>LinkList</div>
+    <div className='output-section'>
+        <h2>Your Sorted Links ({links.length})</h2>
+        {links.length > 0 ? (
+            <ul>
+                {links.map((link, index) =>(
+                    //pass the data for each link down to the LinkItem component
+              <LinkItem key={index} linkdData={link} />
+              ))}
+            </ul>
+        ):(
+            <p>Your processed links wil appear here...</p>
+        )}
+    </div>
   )
 }
 
